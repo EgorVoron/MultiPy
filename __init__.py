2636620f88af61ba16146c29bd9a373009e23fc1
EgorVoron <ekvoron01@gmail.com>
2020-07-31 18:21:30 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from map import map_video
 from filter import filter_video
+from save import save_video
 
-__all__ = ['map_video', 'filter_video']
+__all__ = ['map_video', 'filter_video', 'save_video']
